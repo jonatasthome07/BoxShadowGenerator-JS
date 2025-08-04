4e317c77d127639b77012b18738428b696a63a3e
Jônatas <jonatasthome04@gmail.com>
2025-08-04 16:25:43 -0300

    JS was added using OOP

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Classe
+class BoxShadowGenerator {
+    constructor(horizontal, horizontalRef, vertical, verticalRef, blur, blurRef, spread, spreadRef, box, rule, webkitRule, mozRule){
+        this.horizontal = horizontal
+        this.horizontalRef = horizontalRef
+        this.vertical = vertical
+        this.verticalRef = verticalRef 
+        this.blur = blur
+        this.blurRef = blurRef
+        this.spread = spread
+        this.spreadRef = spreadRef
+        this.box = box
+        this.rule = rule
+        this.webkitRule = webkitRule
+        this.mozRule = mozRule
+    }
+}
+
+//Seleção de elementos[
+const horizontal = document.querySelector("#horizontal")
+const horizontalRef = document.querySelector("#horizontal-value")
+const vertical = document.querySelector("#vertical")
+const verticalRef = document.querySelector("#vertical-value")
+const blur = document.querySelector("#blur")
+const blurRef = document.querySelector("#blur-value")
+const spread = document.querySelector("#spread")
+const spreadRef = document.querySelector("#spread-value")
+const box = document.querySelector("#box")
+const rule = document.querySelector("#rule span")
+const webkitRule = document.querySelector("webkit-rule span")
+const mozRule = document.querySelector("moz-rule span")
+
+
+//Eventos
